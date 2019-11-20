025b2f8d11af1e2ea96fa7c9735ae5fbc96c2dff
vlad-kiriushchenko <vlad.kiruishchenko.adci@gmail.com>
2019-11-20 15:16:20 +0600

    Fix slider

--- src/index.js
+++ src/index.js
@@ -1,22 +1,4 @@
 import "./scss/main.scss";
-import 'slick-carousel/slick/slick.scss'
+import 'slick-carousel/slick/slick.scss';
 import 'slick-carousel/slick/slick';
-
-$(document).ready(function(){
-  $('.projects-slider').slick({
-    dots: true,
-    nextArrow: '.projects-nav-menu__next',
-    prevArrow: '.projects-nav-menu__prev',
-    dotsClass: 'projects-nav-menu-wrap-points',
-    appendDots: '.projects-nav-menu-wrap',
-    customPaging : function(slider, i) {},
-  });
-  $('.projects-slider-item-wrap-slider-nested').slick({
-    accessibility: false,
-    centerMode: true,
-    centerPadding: '150px',
-    draggable: false,
-    prevArrow: null,
-    nextArrow: '<button class="projects-slider-item-wrap-slider-nested-item__btn" aria-label="Next" type="button"></button>',
-  });
-});
+import './js/slider';
---
+++ src/js/slider.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+  $('.projects-slider').slick({
+    dots: true,
+    nextArrow: '.projects-nav-menu__next',
+    prevArrow: '.projects-nav-menu__prev',
+    dotsClass: 'projects-nav-menu-wrap-points',
+    appendDots: '.projects-nav-menu-wrap',
+    customPaging : function(slider, i) {},
+  });
+  $('.projects-slider-item-wrap-slider-nested').slick({
+    accessibility: false,
+    centerMode: true,
+    centerPadding: '150px',
+    draggable: false,
+    prevArrow: null,
+    nextArrow: '<button class="projects-slider-item-wrap-slider-nested-item__btn" aria-label="Next" type="button"></button>',
+  });
+
+
+  var indexSlide = $('.projects-slider').slick('slickCurrentSlide');
+  $('.projects-top-menu__item:eq('+indexSlide+')').addClass('activ-menu');
+
+  $('.projects-slider').on('afterChange', function () {
+    $('.projects-top-menu__item').removeClass('activ-menu');
+    var indexSlide = $('.projects-slider').slick('slickCurrentSlide');
+    $('.projects-top-menu__item:eq('+indexSlide+')').attr('data-index')
+    $('.projects-top-menu__item:eq('+indexSlide+')').addClass('activ-menu');
+  })
+});
