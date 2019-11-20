6ee2888057cd8af4f86270f8fb9ecdd1a654b50e
vlad-kiriushchenko <vlad.kiruishchenko.adci@gmail.com>
2019-11-20 09:58:00 +0600

    Fix slider

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import "./scss/main.scss";
+import 'slick-carousel/slick/slick.scss'
+import 'slick-carousel/slick/slick';
 
-var slider = tns({
-  container: '.projects-slider',
-  items: 1,
-  slideBy: 'page',
-  autoplay: false,
-  autoplayButtonOutput: false,
-  controlsContainer: ".projects-nav-menu",
-  navContainer: ".projects-nav-menu-points"
-
+$(document).ready(function(){
+  $('.projects-slider').slick({
+    dots: true,
+    nextArrow: '.projects-nav-menu__next',
+    prevArrow: '.projects-nav-menu__prev',
+    dotsClass: 'projects-nav-menu-wrap-points',
+    appendDots: '.projects-nav-menu-wrap',
+    customPaging : function(slider, i) {},
+  });
+  $('.projects-slider-item-wrap-slider-nested').slick({
+    accessibility: false,
+    centerMode: true,
+    centerPadding: '150px',
+    draggable: false,
+    prevArrow: null,
+    nextArrow: '<button class="projects-slider-item-wrap-slider-nested-item__btn" aria-label="Next" type="button"></button>',
+  });
 });
-
