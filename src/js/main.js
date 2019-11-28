c59ab542cffd92c4bc6a82969e35fb1136f3fc1c
vlad-kiriushchenko <vlad.kiruishchenko.adci@gmail.com>
2019-11-28 13:03:20 +0600

    Fix responsive

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,12 @@ var headerButton = document.querySelectorAll('.header-footer__button')[0];
 var closeButton = document.querySelectorAll('.leave-request-form-wrap-form__close-btn')[0];
 console.log(headerButton)
 var  popupForm = document.querySelectorAll('.leave-request-form--popup-form')[0];
+
 headerButton.onclick = function() {
   popupForm.classList.add('active-popup')
 };
 
 closeButton.onclick = function() {
   popupForm.classList.toggle('active-popup')
+  return false
 };
