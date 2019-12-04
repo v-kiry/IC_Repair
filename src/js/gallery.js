e5ceccc27faeaecbea3836d7d47fee16ccb5d54f
vlad-kiriushchenko <vlad.kiruishchenko.adci@gmail.com>
2019-12-04 09:58:49 +0600

    Fix realize block

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,17 +1,17 @@
 function gallery() {
-  let arrTypes = []
-
-  document.querySelectorAll('.realize-types__item').forEach((elem, index) => {
-    arrTypes[index] = elem.dataset.type
-  })
-
-
   function changeImg(event) {
     document.querySelectorAll('.realize-types__item').forEach((elem) => {
       elem.classList.remove('active-tab')
     })
     event.target.classList.add('active-tab')
+
     let block = event.target.dataset.type
+    document.querySelectorAll('.realize__content-img').forEach((elem)=>{
+      elem.classList.add('visually-hidden')
+      if (elem.dataset.typeImg == block) {
+        elem.classList.remove('visually-hidden')
+      }
+    })
   }
 
   document.querySelectorAll('.realize-types__item').forEach((elem) => {
