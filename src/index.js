9bb84382abee282f366338d1c04a7e3a3e1d8d06
v-kiry <vkshazs@gmail.com>
2020-04-14 16:55:26 +0600

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import "./scss/main.scss";
-import 'slick-carousel/slick/slick.scss';
-import 'slick-carousel/slick/slick';
-import './js/slider';
-import './js/main';
-import './js/gallery';
-import './js/realizeslider';
-import 'object-fit-polyfill/dist/object-fit-polyfill'
